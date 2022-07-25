c111d887d36322df8fc9723090307ed048770dde
Rob Cohen <robcohen@users.noreply.github.com>
2022-07-25 22:03:07 +0000

    Minor Edits

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
-
 fn main() {
     let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    println!("get_the_weather, {}!", name);
 }
